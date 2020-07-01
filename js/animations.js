a9610efdfe5e9bd8884f8dea73b59811bbf9d866
lautiromero <lautiromero94@gmail.com>
2020-07-01 19:39:59 -0300

    faltan frases servicios

--- js/animations.js
+++ js/animations.js
@@ -9,13 +9,40 @@ $( document ).ready(function() {
 
     $('#s-web')
     .hover(function(){
+        $('.second-sec').css('background-color','#354753');
+        $('#web-ss').css('display','flex');  
+    });
+    $('#s-ec')
+    .hover(function(){
         $('.second-sec').css('background-color','#0e8781');
-        
+        $('#ec-ss').css('display','flex');      
+    });
+    $('#s-so')
+    .hover(function(){
+        $('.second-sec').css('background-color','#a5184e');
+        $('#so-ss').css('display','flex');      
+    });
+    $('#s-di')
+    .hover(function(){
+        $('.second-sec').css('background-color','#966e6a');
+        $('#di-ss').css('display','flex');      
+    });
+    $('#s-vid')
+    .hover(function(){
+        $('.second-sec').css('background-color','#6f91b4');
+        $('#vid-ss').css('display','flex');      
+    });
+    $('#s-bra')
+    .hover(function(){
+        $('.second-sec').css('background-color','#0e8781');
+        $('#bra-ss').css('display','flex');      
     });
 
     $('.services-list a').mouseleave(function(){
         $('.second-sec').css('background-color','#171717');
+        $('.service-box').css('display','none')
     })
+
 
 
 });
