1d4ca2f79e6c42f549279bf4714ed48f5ec47a4b
Lautaro <lautiromero94@gmail.com>
2020-07-09 16:57:10 -0300

    Inicio v. mobile

--- js/animations.js
+++ js/animations.js
@@ -51,4 +51,17 @@ $( document ).ready(function() {
         $(this).css('transform','translate3d(0,0,0)');
     })
 
+
+
+    //Creamos un evento click para el enlace
+    $(".ancla").click(function(evento){
+      //Anulamos la funcionalidad por defecto del evento
+      evento.preventDefault();
+      //Creamos el string del enlace ancla
+      var codigo = "#" + $(this).data("ancla");
+      //Funcionalidad de scroll lento para el enlace ancla en 3 segundos
+      console.log(codigo)
+      $("html,body").animate({scrollTop: $(codigo).offset().top}, 2000);
+    });
+
 });
